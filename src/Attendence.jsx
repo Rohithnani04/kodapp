dcb81a46fcb193c8b1a1ae2bacd1a6d1f56e8f84
Rohithnani04 <rohithnani987@gmail.com>
2024-07-26 15:39:36 +0530

    first commit

---
+++ src/Attendence.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Attendence() {
+  return (
+
+    
+    <h2>Attendence</h2>
+
+
+
+  )
+}
---
+++ src/Classes.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Classes() {
+  return (
+
+    <h2>Classes</h2>
+  )
+}
